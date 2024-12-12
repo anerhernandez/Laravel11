61a7e9b9b018e80fe0a4768463a6aeac52c2a0c3
anerhernandez <anerhernandezl@gmail.com>
2024-12-12 16:30:41 +0000

    Created TaskComponent

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -3,14 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 class DashBoardController extends Controller
 {
     public function index(){
-
-        $user = Auth::user()->name;
-        $tasks = Auth::user()->tasks;
-
-        return view('dashboard', compact('user', 'tasks'));
+        return view('dashboard');
     }
 }
---
+++ resources/views/livewire/task-component.blade.php
@@ -0,0 +1,18 @@
+<div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <div class="text-3xl text-purple-800">
+                        Bienvenido {{$user}}
+                        Usted tiene {{$tasks->count()}} tarea/s creadas
+                    </div>
+                    <div class="border border-purple-400 rounded p-4">
+                        @foreach (Auth::user()->tasks as $task)
+                           <p class="text-green-600">{{$task->title}}</p> <br>
+                           <p class="text-green-400">{{$task->description}}</p> <br>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
